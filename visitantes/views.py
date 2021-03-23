cb00db4672d798437b52728f8d26e6266f116ad3
Camila Adriana <camila.adriana.gomes@outlook.com>
2021-03-23 10:35:20 -0300

    Atualizando, adicionando mensagens de sucesso

--- visitantes/views.py
+++ visitantes/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import render, redirect
 from visitantes.forms import VisitanteForm
 #nome do arquivo + nome da pasta
 
@@ -10,7 +11,20 @@ def registrar_visitante(request):
         form = VisitanteForm(request.POST)
 
         if form.is_valid():
-            form.save()
+            visitante = form.save(commit = False)
+
+            visitante.registrado_por = request.user.porteiro
+            visitante.save()
+
+            messages.success(
+                request, 
+                "Visitante registrado com sucesso!"
+            )
+
+            return redirect("index")
+            #Estou importando o pacote sucess dentro da biblioteca "messages" e estou enviando para o porteiro
+            # a mensagem "visitante registrado com sucesso" 
+
 
     context = {
         "nome_pagina": "Registrar visitante",
@@ -20,5 +34,10 @@ def registrar_visitante(request):
     return render (request, "registrar_visitante.html", context)
 
 
- #O METODO POST É UTILIZADO SEMPRE QUE PRECISAMOS MANDAR INFORMAÇÕES PARA O SERVIDOR E NESSE CASO
+#O METODO POST É UTILIZADO SEMPRE QUE PRECISAMOS MANDAR INFORMAÇÕES PARA O SERVIDOR E NESSE CASO
 #PRECISAMOS ENVIAR AS INFORMAÇÕES DO NOVO VISITANTE CADASTRADO
+
+
+#O MÉTODO COMMIT== FALSE SIGNIFICA QUE O PORTEIRO VAI SER AUTOMATICAMENTE PREENCHIDO A PARTIR DO PORTEIRO REGISTARDO NAQUELE MOMENTO
+#NESSE CASO, ELE PEGA O PORTEIRO LOGADO, COLOCA ELE AUTOMATICAMENTE COMO RESPONSÁVEL PELO VISITANTE E SALVA NO BANCO DE DADOS
+#JÁ O RETURN INDEX SIGINIFICA QUE AS INFORMAÇÕES DO VISITANTE DEVEM ESTAR SENDO EXIBIDAS AUTOMATICAMENTE NA PARTE DA  DASHBOARD
